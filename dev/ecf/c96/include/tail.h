093c3e334a62216ef1d81a2aefc76aafe9455666
Anton Fernando <anton.fernando@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2026-06-17 21:19:57 +0000

    c48_atm: self-contained include dir; document head.h gotcha

--- dev/ecf/c96/include/tail.h
+++
@@ -1,9 +0,0 @@
-# C96 dev override: re-pin ECF_HOST and ECF_PORT in case the job's body
-# loaded prod_envir again and clobbered them.
-unset ECF_HOSTFILE
-export ECF_HOST=%ECF_LOGHOST%
-export ECF_PORT=%ECF_PORT%
-
-timeout 300 ecflow_client --complete  # Notify ecFlow of a normal end
-trap 0                    # Remove all traps
-exit 0                    # End the shell
