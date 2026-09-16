c0cb652751cc1294277f23c128c569077e00326a
Anton Fernando <Anton.Fernando@noaa.gov>
2026-09-16 17:49:45 +0000

    feat(ursa): Update default queue partition to u1-service for C48_ATM
    - Change default QUEUE partition from 'service' to 'u1-service' in bootstrap_ursa.sh
    - Update QUEUE variable default in C48_ATM_ursa.def to 'u1-service'
    - Aligns queue configuration across bootstrap and workflow definition files
    - Ensures consistent HPC partition usage for C48_ATM workflow on Ursa

--- dev/ecf/ursa/bootstrap_ursa.sh
+++ dev/ecf/ursa/bootstrap_ursa.sh
@@ -74,7 +74,7 @@ ecflow_client --alter add variable EXPDIR      "${EXPDIR}"      "${SUITE}"
 ecflow_client --alter add variable COMROOT     "${COMROOT}"     "${SUITE}"
 ecflow_client --alter add variable PSLOT       "${PSLOT}"       "${SUITE}"
 ecflow_client --alter add variable ACCOUNT     "${HPC_ACCOUNT:-fv3-cpu}" "${SUITE}"
-ecflow_client --alter add variable QUEUE        "${PARTITION_BATCH:-service}" "${SUITE}"
+ecflow_client --alter add variable QUEUE        "${PARTITION_BATCH:-u1-service}" "${SUITE}"
 
 echo "  Variables set."
 
